b89073b6a0d0a8de198417965e315de8698d5fd4
Augusto Andrade <webgutodev@gmail.com>
2021-09-24 10:56:19 -0300

    Exercicio retangulo

---
+++ RetanguloOp/RetanguloOp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RetanguloOp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo r1 = new Retangulo();
+            
+            Console.WriteLine("Entre com os valores do retangulo:");
+            Console.Write("Largura : ");
+            r1.L = double.Parse(Console.ReadLine());
+            Console.Write("Altura : ");
+            r1.A = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(r1);
+
+        }
+    }
+}
---
+++ RetanguloOp/RetanguloOp/Retangulo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace RetanguloOp
+{
+    class Retangulo
+    {
+        public double L, A;        
+
+        public double Area()
+        {            
+            return L * A;
+        }
+
+        public double Perimetro()
+        {
+            return (2 * L) + (2 * A);
+        }
+
+        public double Diagonal()
+        {
+            return Math.Sqrt(Math.Pow(L, 2) + Math.Pow(A, 2));
+        }
+
+        public override string ToString()
+        {
+            return 
+                "Area = "
+                +Area().ToString("F2", CultureInfo.InvariantCulture)
+                +"\nPerimetro = "
+                +Perimetro().ToString("F2", CultureInfo.InvariantCulture)
+                +"\nDiagonal = "
+                +Diagonal().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
+
+
+/* 
+ * Area = L  * A;
+ * Perimetro = 2*L + 2*A
+ * Diagonal = Raiz de  2*L + 2*A
+ */
