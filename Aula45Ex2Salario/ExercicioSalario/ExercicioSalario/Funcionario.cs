9ccc2b5ae3ab6a6090aac19813dbd3b65e751a94
Augusto Andrade <webgutodev@gmail.com>
2021-09-24 13:20:26 -0300

    Aula 45 exercicio 02.

---
+++ Aula45Ex2Salario/ExercicioSalario/ExercicioSalario/Funcionario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace ExercicioSalario
+{
+    class Funcionario
+    {
+        public String Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+
+        public void AumentarSalario(double porcentagem)
+        {
+
+            SalarioBruto += ((porcentagem / 100.0) * SalarioBruto);
+        }
+        public override string ToString()
+        {
+            return Nome
+                    + ",  $ "
+                    + SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Aula45Ex2Salario/ExercicioSalario/ExercicioSalario/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioSalario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario f1 = new Funcionario();
+            Console.WriteLine("Entre com os dados do funcionario: ");
+            Console.Write("Nome: ");
+            f1.Nome = Console.ReadLine();
+            Console.Write("Salario Bruto: ");
+            f1.SalarioBruto = double.Parse(Console.ReadLine());
+            Console.Write("Imposto: ");
+            f1.Imposto = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Funcionario: " + f1);
+
+            Console.WriteLine("Digite a porcentagem para aumentar o salario: ");
+            f1.AumentarSalario(double.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Dados Atualizados: " + f1);
+        }
+    }
+}
