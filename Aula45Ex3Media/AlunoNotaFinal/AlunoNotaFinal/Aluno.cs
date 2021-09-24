554ddcf3291f599875e25498c60012a9d1938ed8
Augusto Andrade <webgutodev@gmail.com>
2021-09-24 14:39:26 -0300

    AULA 45 - EXERCICIO 3 - APROVADO REPROVADO

---
+++ Aula45Ex3Media/AlunoNotaFinal/AlunoNotaFinal/Aluno.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace AlunoNotaFinal
+{
+    class Aluno
+    {
+        public string Nome;
+        public double N1, N2, N3;
+
+        public double NotaFinal()
+        {            
+            return N1 + N2 + N3;
+        }
+     
+        public override string ToString()
+        {
+            if (NotaFinal() > 60)
+            {
+                return $"NOTA FINAL = {NotaFinal().ToString("F2", CultureInfo.InvariantCulture)} \nAPROVADO!";
+            }
+            else return $"NOTA FINAL = {NotaFinal().ToString("F2", CultureInfo.InvariantCulture)} \nREPROVADO!\nFALTARAM {(60.00 - NotaFinal()).ToString(CultureInfo.InvariantCulture)} PONTOS";
+        }
+    }
+}
---
+++ Aula45Ex3Media/AlunoNotaFinal/AlunoNotaFinal/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace AlunoNotaFinal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno a1 = new Aluno();
+            Console.WriteLine("Entre com os dados do aluno: ");
+            Console.Write("Nome:");
+            a1.Nome = Console.ReadLine();
+
+            Console.Write("Nota primeiro Trimestre: ");
+            a1.N1 = double.Parse(Console.ReadLine());
+            Console.Write("Nota segundo Trimestre: ");
+            a1.N2 = double.Parse(Console.ReadLine());
+            Console.Write("Nota terceiro Trimestre: ");
+            a1.N3 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(a1);
+        }
+    }
+}
