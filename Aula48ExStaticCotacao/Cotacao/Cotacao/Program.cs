bed79414d1cbe4a115cf3107251ced3dd91f8134
Augusto Andrade <webgutodev@gmail.com>
2021-09-27 14:00:31 -0300

    alteração. Aula 48, comprar dolar.

--- Aula48ExStaticCotacao/Cotacao/Cotacao/Program.cs
+++ Aula48ExStaticCotacao/Cotacao/Cotacao/Program.cs
@@ -10,7 +10,7 @@ namespace Cotacao
             double total;
             double qtComprar;
             Console.Write("Quantos dolares voce vai comprar: ");
-            qtComprar = double.Parse(Console.ReadLine());
+            qtComprar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
             total = Cotacao.Conversao(qtComprar);
             Console.WriteLine("Valor a ser pago em reais = " + total.ToString("F2", CultureInfo.InvariantCulture));
