90df41924c6fcc15a6fc9e3283bace8149a31151
Augusto Andrade <webgutodev@gmail.com>
2021-09-27 13:35:59 -0300

    Exercicio aula 48. Compra dolar

---
+++ Aula48ExStaticCotacao/Cotacao/Cotacao/Cotacao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cotacao
+{
+    class Cotacao
+    {
+        public static double Dolar = 3.10;
+
+        public static double Conversao(double valor)
+        {
+            return (valor + valor * 0.06) * Dolar;
+        }
+    }
+}
---
+++ Aula48ExStaticCotacao/Cotacao/Cotacao/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace Cotacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double total;
+            double qtComprar;
+            Console.Write("Quantos dolares voce vai comprar: ");
+            qtComprar = double.Parse(Console.ReadLine());
+
+            total = Cotacao.Conversao(qtComprar);
+            Console.WriteLine("Valor a ser pago em reais = " + total.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
