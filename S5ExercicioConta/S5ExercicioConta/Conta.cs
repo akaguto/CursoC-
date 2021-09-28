2ef8f8282deed02718bca7f4f7c8761cd98e9471
Augusto Andrade <webgutodev@gmail.com>
2021-09-28 16:19:33 -0300

    Primeira versão, sem correção.

---
+++ S5ExercicioConta/S5ExercicioConta/Conta.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace S5ExercicioConta
+{
+    class Conta
+    {
+        public string Nome { get; set; }
+        public int NConta { get; private set; }
+        public double Saldo { get; private set; }
+
+        public Conta(int numero, string nome, double saldo)
+        {
+            NConta = numero;
+            Nome = nome;
+            Saldo = saldo;
+        }
+
+        public void Deposita(double valor)
+        {
+            Saldo += valor;
+        }
+
+        public void Saca(double valor)
+        {
+            Saldo -= valor;
+            Saldo -= 5.0;
+        }
+
+        public override string ToString()
+        {
+            return
+                "Conta: " 
+                + NConta
+                +", Titular: " 
+                + Nome
+                +", Saldo: $"
+                +Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ S5ExercicioConta/S5ExercicioConta/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Globalization;
+
+namespace S5ExercicioConta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroConta;
+            string nome;
+            char condicao;
+            double deposito;
+
+            Conta cliente1;
+            Console.WriteLine("Digite os dados da conta: ");
+            Console.Write("Número da conta: ");
+            numeroConta = int.Parse(Console.ReadLine());
+            Console.Write("Nome do titular: ");
+            nome = Console.ReadLine();
+            Console.Write("Haverá depósito inicial: (s/n)?");
+            condicao = char.Parse(Console.ReadLine());
+
+            if(condicao == 's' || condicao == 'S')
+            {
+                Console.Write("Valor de deposito inicial: ");
+                deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                cliente1 = new Conta(numeroConta, nome, deposito);
+            }
+            else
+            {
+                deposito = 0.0;
+                cliente1 = new Conta(numeroConta, nome, deposito);
+            }
+
+            Console.WriteLine(" ");
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(cliente1);
+
+            Console.WriteLine(" ");
+            Console.Write("Entre com um valor para o depósito: ");
+            double valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            cliente1.Deposita(valor);
+            Console.WriteLine(" ");
+            Console.WriteLine("Dados da conta Atualizados: ");
+            Console.WriteLine(cliente1);
+
+            Console.WriteLine(" ");
+            Console.Write("Entre com um calor para saque: ");
+            valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Dados da conta Atualizados: ");
+            cliente1.Saca(valor);
+            Console.WriteLine(cliente1);
+        }
+    }
+}
