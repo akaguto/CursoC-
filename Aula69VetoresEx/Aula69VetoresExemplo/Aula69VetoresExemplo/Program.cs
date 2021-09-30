213d065e27ed1970971121ba22f1d0357a473809
Augusto Andrade <webgutodev@gmail.com>
2021-09-30 15:06:03 -0300

    Aula 68 Nullable.

---
+++ Aula69VetoresEx/Aula69VetoresExemplo/Aula69VetoresExemplo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace Aula69VetoresExemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o numero de alturas que deseja armazenar: ");
+            int n = int.Parse(Console.ReadLine());
+            double[] alturas = new double[n];
+
+            double soma = 0.0;
+
+            for(int i = 0; i < n; i++)
+            {
+                Console.Write("Digite a altura: ");
+                alturas[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            foreach(double altura in alturas)
+            {
+                soma += altura;
+            }
+
+            double media = soma / n;
+
+            Console.WriteLine("A média das alturas é: " + media);
+        }
+    }
+}
