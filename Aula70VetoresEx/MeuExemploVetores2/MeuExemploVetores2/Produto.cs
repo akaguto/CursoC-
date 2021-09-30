cc988989bc622a5eb2eb313c1a5516781c027a5b
Augusto Andrade <webgutodev@gmail.com>
2021-09-30 16:58:40 -0300

    Meu Exemplo. Aula 2: vetores.

---
+++ Aula70VetoresEx/MeuExemploVetores2/MeuExemploVetores2/Produto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MeuExemploVetores2
+{
+    class Produto
+    {
+        public string Nome { get; set; }
+        public double Preco { get; private set; }
+
+        public Produto(string nome, double preco)
+        {
+            Nome = nome;
+            Preco = preco;
+        }
+    }
+}
---
+++ Aula70VetoresEx/MeuExemploVetores2/MeuExemploVetores2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace MeuExemploVetores2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double soma = 0.0;
+            double media;
+            
+            Console.Write("Digite a quantidade de produtos que deseja adicionar: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Produto[] produto = new Produto[n];
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine(" ");
+                Console.Write("Informe o nome do produto: ");
+                string nome = Console.ReadLine();
+                Console.Write("Informe o preco do produto: ");
+                double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                produto[i] = new Produto(nome, preco);
+            }
+
+            for(int i = 0; i < n; i++)
+            {
+                soma += produto[i].Preco; 
+            }
+
+            media = soma / n;
+            Console.WriteLine(" ");
+            Console.WriteLine("O preço médio dos produtos é: " + media.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
