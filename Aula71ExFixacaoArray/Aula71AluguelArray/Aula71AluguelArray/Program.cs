8fa6219e51ba1816e8f12163031788b747e070b9
Augusto Andrade <webgutodev@gmail.com>
2021-10-04 15:54:52 -0300

    Aula 71: Exercício Array Classe

---
+++ Aula71ExFixacaoArray/Aula71AluguelArray/Aula71AluguelArray/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+
+namespace Aula71AluguelArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Quarto[] quarto = new Quarto[11];
+            Console.Write("Quantos quartos serão alugados: ");
+            int n = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Informe o numero do quarto que será alugado: ");
+                int numero = int.Parse(Console.ReadLine());
+                Console.Write("Informe o nome do cliente: ");
+                string nome = Console.ReadLine();
+                Console.Write("Informe o E-mail do cliente: ");
+                string email = Console.ReadLine();
+                Console.WriteLine(" ");
+
+                quarto[numero] = new Quarto(nome, email, numero);
+            }
+
+            for (int i = 1; i < 11; i++)
+                if (quarto[i] != null)
+                {
+                    Console.WriteLine("Aluguel #" + i);
+                    Console.WriteLine(quarto[i]);
+                    Console.WriteLine("");
+                }
+            Console.WriteLine(" ");
+        }
+    }
+}
---
+++ Aula71ExFixacaoArray/Aula71AluguelArray/Aula71AluguelArray/Quarto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula71AluguelArray
+{
+    class Quarto
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public int Numero { get; set; }
+        public Quarto(string nome, string email, int numero)
+        {
+            Nome = nome;
+            Email = email;
+            Numero = numero;
+        }
+
+        public override string ToString()
+        {
+            return "Nome: " 
+                +Nome
+                +"\nE-mail: "
+                +Email
+                +"\nQuarto: "
+                +Numero;
+        }
+    }
+}
