396b3b009cc3b6c6f4f57043510ba94d8e05aa53
Augusto Andrade <webgutodev@gmail.com>
2021-10-18 16:17:38 -0300

    Aula 72: Params.

--- Aula72Params/Params/Params/Program.cs
+++ Aula72Params/Params/Params/Program.cs
@@ -6,10 +6,10 @@ namespace Params
     {
         static void Main(string[] args)
         {
-            int result = Calculator.Sum(10, 20, 30, 40);
+            int result = Calculator.Sum(10, 20, 30, 30);
             Console.WriteLine(result);
 
-            int result2 = Calculator.Sum(13, 20, 30, 40);
+            int result2 = Calculator.Sum(13, 20, 340, 40);
             Console.WriteLine(result2);
         }
     }
