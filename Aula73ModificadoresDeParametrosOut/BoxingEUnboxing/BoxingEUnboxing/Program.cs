ff264c133e9dd94759116ac61f27c63c21cc02af
Augusto Andrade <webgutodev@gmail.com>
2021-10-20 14:58:55 -0300

    Aula 74: Boxing e Unboxing.

---
+++ Aula73ModificadoresDeParametrosOut/BoxingEUnboxing/BoxingEUnboxing/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BoxingEUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 20;
+            Object obj = x; //Boxing: É o processo de conversão de um objeto tipo valor para um objeto tipo referência compatível.
+
+            int y = (int) obj; //Unboxing: É o processo de conversão de um objeto tipo referência para um objeto tipo valor compatível.
+            Console.WriteLine("Boxing: Obj = " + obj);
+            Console.WriteLine("Unboxing: y = " + y);
+        }
+    }
+}
