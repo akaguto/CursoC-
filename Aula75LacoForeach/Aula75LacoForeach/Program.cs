f0cb649e298c934460814d344b7a7d4df34abd54
Augusto Andrade <webgutodev@gmail.com>
2021-10-20 15:24:01 -0300

    Aula 75: Laço foreach

---
+++ Aula75LacoForeach/Aula75LacoForeach/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Aula75LacoForeach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vect = new string[] { "Pantera", "Tool", "Metallica" };
+
+            Console.WriteLine("----//for//----");
+            for (int i = 0; i < vect.Length; i++)
+            {
+                Console.WriteLine(vect[i]);
+            }
+
+            Console.WriteLine("\n----//foreach//----");                        
+            foreach (string obj in vect)
+            {
+                Console.WriteLine(obj);
+            }
+            
+        }
+    }
+}
